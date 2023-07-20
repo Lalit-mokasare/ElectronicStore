3f0b366cfebc2e930cad2d8229e9661a695c9d76
DELL <lmokasare@gmail.com>
2023-07-20 01:47:27 -0700

    User module validation started

--- src/main/java/com/lcwd/electronic/store/dtos/UserDto.java
+++ src/main/java/com/lcwd/electronic/store/dtos/UserDto.java
@@ -1,7 +1,6 @@
 package com.lcwd.electronic.store.dtos;
 
 
-
 import lombok.*;
 import org.aspectj.bridge.IMessage;
 
@@ -23,15 +22,16 @@ import javax.validation.constraints.Size;
 public class UserDto {
     private String userId;
 
-    @Size(min = 3,max = 15,message = "Invalid name..!!")
+
+    @Size(min = 3, max = 15, message = "Invalid name..!!")
     private String name;
-    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+\\.)[a-z]{2,5}$",message = "Invalid mail address")
+    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+\\.)[a-z]{2,5}$", message = "Invalid mail address")
     @Email(message = "Invalid email address..!!")
-    @NotBlank (message = "Email is required..!!")
+    @NotBlank(message = "Email is required..!!")
     private String email;
     @NotBlank(message = "Password is required..!!")
     private String password;
-    @Size(min = 4,max = 6,message = "Invalid gender..!!")
+    @Size(min = 4, max = 6, message = "Invalid gender..!!")
     private String gender;
     @NotBlank(message = "Write  something about yourself..!!")
     private String about;
